3c7434ab9987f053f05fdab3cc56f6db16d4613a
hansrajathole <hansrajathole1@gmail.com>
2025-07-27 08:43:04 +0530

    Update database connection string to use MongoDB Atlas

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const connect = ()=>{
-    mongoose.connect("mongodb://localhost:27017/VIPS")
+    mongoose.connect("mongodb+srv://atholehansraj72:u2VuIOC00wRFxP8t@cluster0.saxvgwo.mongodb.net/VIPS")
     .then(()=>{
         console.log("database connected");
     })
