d6aac3cac53f4d6781631cc8807a08593fc1e31d
hansrajathole <hansrajathole1@gmail.com>
2025-07-27 08:48:17 +0530

    Refactor server.js to define port as a constant for improved readability

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./src/app")
 const connect = require("./src/db/db")
-
-app.listen(3000, ()=>{
-    console.log("server running on port no : ",3000);
+const port = 3000
+app.listen(port, ()=>{
+    console.log("server running on port no : ",port);
     connect()
 })
